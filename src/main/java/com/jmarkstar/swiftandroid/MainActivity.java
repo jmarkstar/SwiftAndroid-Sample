d8f4eac3b030ffe446778aa068a3ec0aa927567c
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-17 14:05:34 -0500

    Working on List View Adapters

--- src/main/java/com/jmarkstar/swiftandroid/MainActivity.java
+++ src/main/java/com/jmarkstar/swiftandroid/MainActivity.java
@@ -1,30 +1,32 @@
 package com.jmarkstar.swiftandroid;
 
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ListView;
+import java.util.List;
+import java.util.zip.Inflater;
 
-import com.johnholdsworth.swiftbindings.SwiftMathBinding;
+import com.johnholdsworth.swiftbindings.SwiftListViewBinding;
 
-public class MainActivity extends AppCompatActivity implements SwiftMathBinding.Responder {
-
-    static SwiftMathBinding.Listener listener;
+public class MainActivity extends AppCompatActivity {
 
     private static void loadNativeDependencies() {
         System.loadLibrary("swiftandroid");
     }
 
-    private EditText etNum1, etNum2;
-    private Button btnSum;
-    private TextView tvResult;
-
-    /** Implemented in src/main/swift/Sources/main.swift */
-    @SuppressWarnings("JniMissingFunction")
-    native SwiftMathBinding.Listener bind(SwiftMathBinding.Responder self );
+    private ListView tableView;
+    private SwiftAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +34,11 @@ public class MainActivity extends AppCompatActivity implements SwiftMathBinding.
         setContentView(R.layout.activity_main);
 
         loadNativeDependencies();
-        listener = bind( this );
 
-        etNum1 = (EditText)findViewById(R.id.et_num1);
-        etNum2 = (EditText)findViewById(R.id.et_num2);
-        btnSum = (Button)findViewById(R.id.btn_sum);
-        tvResult = (TextView)findViewById(R.id.tv_result);
-
+        tableView = (ListView) findViewById(R.id.tableView);
+        adapter = SwiftAdapter.newInstance(this);
+        tableView.setAdapter(adapter);
+        /*
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,11 +57,87 @@ public class MainActivity extends AppCompatActivity implements SwiftMathBinding.
 
                 listener.processSum(num1, num2);
             }
-        });
+        });*/
     }
 
     //Response from Swift
+    /*
     @Override public void processedSum(int result) {
         tvResult.setText(String.valueOf(result));
+    }*/
+}
+
+class SwiftAdapter extends BaseAdapter implements SwiftListViewBinding.Responder {
+
+    SwiftListViewBinding.Listener listener;
+
+    /** Implemented in src/main/swift/Sources/main.swift */
+    @SuppressWarnings("JniMissingFunction")
+    private native SwiftListViewBinding.Listener bind(SwiftListViewBinding.Responder self );
+
+    private LayoutInflater inflater;
+
+    public static SwiftAdapter newInstance(Context context) {
+
+        SwiftAdapter newInstance = new SwiftAdapter();
+        newInstance.bind(newInstance);
+        newInstance.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        return newInstance;
+    }
+
+    // adapter
+
+    @Override
+    public int getCount() {
+        return listener.numberOfRows();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view;
+        CustomCell cell;
+
+        // create
+        if (convertView == null) {
+
+            view = inflater.inflate(R.id.cell, parent, false);
+
+            cell = new CustomCell();
+            cell.textView = (TextView) view.findViewById(R.id.textView);
+
+            view.setTag(cell);
+
+        } else {
+
+            view = convertView;
+            cell = (CustomCell) view.getTag();
+        }
+
+        // configure
+        listener.configureCell(cell, position);
+
+        return convertView;
+    }
+
+    class CustomCell implements SwiftListViewBinding.Cell {
+
+        TextView textView;
+
+        @Override
+        public void setTitle(String title) {
+
+            textView.setText(title);
+        }
     }
 }
--- src/main/swift/Sources/main.swift
+++ src/main/swift/Sources/main.swift
@@ -12,14 +12,20 @@ import Foundation
 public func bind_samples( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self: jobject? )-> jobject? {
 
     // This Swift instance forwards to Java through JNI
-    responder = SwiftMathBinding_ResponderForward( javaObject: __self )
+    let responder = SwiftMathBinding_ResponderForward( javaObject: __self )
 
     // This Swift instance receives native calls from Java
     var locals = [jobject]()
-    return SwiftMathBinding_ListenerImpl().localJavaObject( &locals )
+    return SwiftMathBinding_ListenerImpl(responder: responder).localJavaObject( &locals )
 }
 
 final class SwiftMathBinding_ListenerImpl: SwiftMathBinding_ListenerBase {
+    
+    init(responder: SwiftMathBinding_ResponderForward) {
+        self.responder = responder
+    }
+    
+    let responder: SwiftMathBinding_ResponderForward
 
     override func processSum( num1: Int, num2: Int ) {
         
@@ -30,3 +36,39 @@ final class SwiftMathBinding_ListenerImpl: SwiftMathBinding_ListenerBase {
         responder.processedSum(result: result)
     }
 }
+
+// one-off call to bind the Java and Swift sections of app
+@_silgen_name("Java_com_jmarkstar_swiftandroid_SwiftAdapter_bind")
+public func bind_adapter( __env: UnsafeMutablePointer<JNIEnv?>, __this: jobject?, __self: jobject? )-> jobject? {
+
+    // This Swift instance forwards to Java through JNI
+    let responder = SwiftListViewBinding_ResponderForward( javaObject: __self )
+    
+    // This Swift instance receives native calls from Java
+    var locals = [jobject]()
+    return SwiftListViewBinding_ListenerImpl(responder: responder).localJavaObject( &locals )
+}
+
+final class SwiftListViewBinding_ListenerImpl: SwiftListViewBinding_ListenerBase {
+    
+    init(responder: SwiftListViewBinding_ResponderForward) {
+        self.responder = responder
+    }
+    
+    let responder: SwiftListViewBinding_ResponderForward
+    
+    override func numberOfRows() -> Int {
+        
+        NSLog("Swift Adapter: \(#function)")
+        
+        return 5
+    }
+    
+    override func configureCell(cell: SwiftListViewBinding_Cell?, row: Int) {
+        
+        NSLog("Swift Adapter: \(#function)")
+        
+        cell?.setTitle(title: "Swift test \(row)")
+    }
+}
+
