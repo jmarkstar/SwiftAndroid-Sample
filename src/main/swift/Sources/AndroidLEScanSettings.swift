6641f38ecf9dde745b6512550113772748d2b431
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-20 03:27:32 -0500

    Working on JNI

--- src/main/swift/Sources/AndroidLEScanSettings.swift
+++ src/main/swift/Sources/AndroidLEScanSettings.swift
@@ -52,6 +52,23 @@ public final class AndroidBluetoothLowEnergyScanSettings: JavaObject {
         
         JNI.DeleteLocalRef( __object )
     }
+    
+    private static var SCAN_MODE_BALANCED_MethodID: jmethodID?
+    
+    fileprivate static var SCAN_MODE_BALANCED: Int {
+        
+        get {
+            
+            let __value = JNIField.GetStaticIntField(
+                fieldName: "SCAN_MODE_BALANCED",
+                fieldType: "I",
+                fieldCache: &SCAN_MODE_BALANCED_MethodID,
+                className: javaClassName,
+                classCache: &JNIClass )
+            
+            return Int(__value)
+        }
+    }
 }
 
 public final class AndroidBluetoothLowEnergyScanSettingsBuilder: JavaObject {
@@ -94,5 +111,75 @@ public final class AndroidBluetoothLowEnergyScanSettingsBuilder: JavaObject {
         self.init( javaObject: __object )
         
         JNI.DeleteLocalRef( __object )
+    }
+    
+    private static var build_MethodID: jmethodID?
+    
+    public func build() -> AndroidBluetoothLowEnergyScanSettings {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue]()
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "build",
+                                                  methodSig: "()Landroid/bluetooth/le/ScanSettings;",
+                                                  methodCache: &AndroidBluetoothLowEnergyScanSettingsBuilder.build_MethodID,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        
+        defer { JNI.DeleteLocalRef( __return ) }
+
+        return AndroidBluetoothLowEnergyScanSettings( javaObject: __return )
+    }
+    
+    private static var setScanMode_MethodID: jmethodID?
+    
+    // public android.bluetooth.le.ScanSettings$Builder setScanMode(int);
+    // descriptor: (I)Landroid/bluetooth/le/ScanSettings$Builder;
+    public func setScanMode(_ newValue: AndroidBluetoothLowEnergyScanSettings.ScanMode) -> AndroidBluetoothLowEnergyScanSettingsBuilder {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue]( repeating: jvalue(), count: 1 )
+        
+        __args[0] = jvalue(i: jint(newValue.rawValue))
+        
+        JNIMethod.CallVoidMethod(
+            object: javaObject,
+            methodName: "setScanMode",
+            methodSig: "(I)Landroid/bluetooth/le/ScanSettings$Builder;",
+            methodCache: &AndroidBluetoothLowEnergyScanSettingsBuilder.setScanMode_MethodID,
+            args: &__args,
+            locals: &__locals )
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "setScanMode",
+                                                  methodSig: "(I)Landroid/bluetooth/le/ScanSettings$Builder;",
+                                                  methodCache: &AndroidBluetoothLowEnergyScanSettingsBuilder.setScanMode_MethodID,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        
+        defer { JNI.DeleteLocalRef( __return ) }
+        
+        return AndroidBluetoothLowEnergyScanSettingsBuilder(javaObject: __return)
+    }
+}
+
+public extension AndroidBluetoothLowEnergyScanSettings {
+    
+    public struct ScanMode: RawRepresentable {
+        
+        public let rawValue: Int
+        
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+        
+        /**
+         * Perform Bluetooth LE scan in balanced power mode. Scan results are returned at a rate that
+         * provides a good trade-off between scan frequency and power consumption.
+         */
+        public static let balanced = ScanMode(rawValue: AndroidBluetoothLowEnergyScanSettings.SCAN_MODE_BALANCED)
     }
 }
